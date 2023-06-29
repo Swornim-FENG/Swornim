2f6e42ab7ec87c396cc8d062dcf71c9df5c1614b
Rikshal <rikshalstha20@gmail.com>
2023-06-29 13:01:24 +0545

    Updated About Us page

---
+++ resources/views/About_us.blade.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>About MSH</title>
+  <link rel="stylesheet" type="text/css" href="styles.css">
+<style>
+  /* styles.css */
+body {
+  font-family: Arial, sans-serif;
+  margin: 0;
+  padding: 0;
+}
+
+header {
+  background-color: #ff6000;
+  color: #fff;
+  padding: 10px;
+  margin : 0 20px;
+  padding : 20px 30px;
+}
+
+nav ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+}
+
+nav ul li {
+  display: inline-block;
+  margin-right: 10px;
+}
+
+nav ul li a {
+  color: white;
+  text-decoration: none;
+}
+
+section.about {
+  background-color: #f9f9f9;
+  padding: 50px;
+}
+
+.container {
+  max-width: 800px;
+  margin: 0 auto;
+  flex : 1;
+}
+
+h1 {
+  font-size: 36px;
+  margin-bottom: 20px;
+}
+
+p {
+  font-size: 16px;
+  line-height: 1.5;
+  margin-bottom: 10px;
+}
+
+a.button {
+  display: inline-block;
+  padding: 10px 20px;
+  background-color: #000;
+  color: #fff;
+  text-decoration: none;
+  border-radius: 4px;
+  
+}
+.a.button:hover{
+    color : white;
+    background-color : #ff6000;
+}
+
+
+footer {
+  background-color: #ff6000;
+  color: white;
+  padding: 10px;
+  text-align: center;
+  
+}
+.container{
+  margin-bottom : 160px;
+  
+}
+
+</style>
+</head>
+<body>
+  <header>
+      <ul>
+        <li>Home</li>
+        <li>Products</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+  </header>
+
+  <section class="about">
+    <div class="container">
+      <h1>About MSH</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla porttitor ante at leo eleifend, ac rutrum tortor maximus. Integer lobortis ipsum nec finibus fermentum. Curabitur vulputate rutrum dolor in mattis. Duis lacinia dignissim eros, nec iaculis quam gravida id.</p>
+      <p>Nulla lobortis ex sapien, vitae tempus lacus rutrum eget. Ut aliquet tristique ante, eget pharetra nisl efficitur et. Integer facilisis, sapien in malesuada venenatis, turpis dolor pellentesque sem, nec ultricies enim est eget velit. Nam accumsan vestibulum nisl sed vestibulum.</p>
+      <a href="" class="button">Contact Us</a>
+    </div>
+  </section>
+
+  <footer>
+    <p>&copy; 2023 Your Company. All rights reserved.</p>
+  </footer>
+</body>
+</html>
