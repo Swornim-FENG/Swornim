03e7c61560840e45e3ad4faa5a3d32506397ec1f
Your Name <aarpanadhikari979@gmail.com>
2023-07-13 19:29:17 +0545

    web

--- routes/web.php
+++ routes/web.php
@@ -6,13 +6,14 @@ use App\Http\Controllers\SignupController;
 
 
 Route::get('/signup', [SignupController::class, 'signup']);
+
+
 Route::get('/login', function () {
-    return view('login');
-});
-Route::get('/login.html', function () {
+
     return view('login');
 });
 Route::post('/signup', [SignupController::class, 'validate_signup']);
+
 Route::get('/product-page', function () {
     return view('product-page');
 });
